1ee5fa5ce91bc9ce1f30c6bf11f27f22b17d5c32
Caio Nikolas <caio.kanu@hotmail.com>
2024-07-23 22:39:33 -0300

    fix

--- Backend/prisma/migrations/20240724013855_sqlite_db/migration.sql
+++ Backend/prisma/migrations/20240724013855_sqlite_db/migration.sql
@@ -1,31 +1,25 @@
 -- CreateTable
 CREATE TABLE "raffle" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "maximum_numbers" INTEGER NOT NULL,
     "draw_date" TEXT,
-    "prize_pool" TEXT,
-
-    CONSTRAINT "raffle_pkey" PRIMARY KEY ("id")
+    "prize_pool" TEXT
 );
 
 -- CreateTable
 CREATE TABLE "ticket" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "number" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT,
     "cel_number" TEXT,
     "paid" BOOLEAN,
     "raffle_id" TEXT NOT NULL,
-
-    CONSTRAINT "ticket_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ticket_raffle_id_fkey" FOREIGN KEY ("raffle_id") REFERENCES "raffle" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ticket_raffle_id_number_key" ON "ticket"("raffle_id", "number");
-
--- AddForeignKey
-ALTER TABLE "ticket" ADD CONSTRAINT "ticket_raffle_id_fkey" FOREIGN KEY ("raffle_id") REFERENCES "raffle"("id") ON DELETE CASCADE ON UPDATE CASCADE;
